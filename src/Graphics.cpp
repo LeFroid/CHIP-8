a3e7d8b86f7510175c0d3621d8ef3c7733651853
Le Froid <tmvfroid@gmail.com>
2016-08-01 08:44:28 -0400

    Run timer in its own thread for better performance

--- src/Graphics.cpp
+++ src/Graphics.cpp
@@ -1,10 +1,36 @@
+/*
+ * Copyright (c) 2016, Timothy Vaccarelli
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ */
+
 #include "Graphics.h"
 
 #include <cstring>
 
 Graphics::Graphics() :
-    m_window(0),
-    m_renderer(0)
+    m_window(nullptr),
+    m_renderer(nullptr)
 {
     SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
 
@@ -14,12 +40,16 @@ Graphics::Graphics() :
 Graphics::~Graphics()
 {
     if (m_renderer)
+    {
         SDL_DestroyRenderer(m_renderer);
+        m_renderer = nullptr;
+    }
     
     if (m_window)
+    {
         SDL_DestroyWindow(m_window);
-
-    SDL_Quit();
+        m_window = nullptr;
+    }
 }
 
 void Graphics::createWindow(int width, int height, int scale)
